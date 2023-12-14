872f2cae2d67e83438714382c53db21adc96b548
Abrorbek Murodaliyev <abrordev2004@gmail.com>
2023-12-14 11:51:26 +0500

    login
    login sahifasini sozladim

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,13 +1,14 @@
 <!doctype html>
 <html class="no-js " lang="en">
 
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
 
-    <title>Login</title>
+    <title>404</title>
     <!-- Favicon-->
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     <!-- Custom Css -->
@@ -21,26 +22,17 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-4 col-sm-12">
-                    <form class="card auth_form" action="{{ route('login') }}" method="POST">
-                        @csrf
+                    <form class="card auth_form">
                         <div class="header">
-                            <img class="logo" src="{{ asset('admin/assets/images/logo.svg') }}" alt="">
-                            <h5>Log in</h5>
+                            <img class="logo" src="assets/images/logo.svg" alt="">
+                            <h5>Error 500</h5>
+                            <span>Something's broken :-(</span>
                         </div>
                         <div class="body">
-                            <div class="input-group mb-3">
-                                <input type="email" class="form-control" placeholder="Username" name="email" required>
-                                <div class="input-group-append">
-                                    <span class="input-group-text"><i class="zmdi zmdi-account-circle"></i></span>
-                                </div>
+                            <a href="/" class="btn btn-primary btn-block waves-effect waves-light">GO TO HOMEPAGE</a>
+                            <div class="signin_with mt-3">
+                                <a href="javascript:void(0);" class="link">Need Help?</a>
                             </div>
-                            <div class="input-group mb-3">
-                                <input type="text" class="form-control" placeholder="Password" name="password" required>
-                                <div class="input-group-append">
-                                    <span class="input-group-text"><a href="forgot-password.html" class="forgot" title="Forgot Password"><i class="zmdi zmdi-lock"></i></a></span>
-                                </div>
-                            </div>
-                            <button class="btn btn-primary btn-block waves-effect waves-light" type="submit">SIGN IN</button>
                         </div>
                     </form>
                     <div class="copyright text-center">
@@ -48,12 +40,12 @@
                         <script>
                             document.write(new Date().getFullYear())
                         </script>,
-                        <span><a href="templatespoint.net">Templates Point</a></span>
+                        <span><a href="https://t.me/Abrordev_sw">Abrordev</a></span>
                     </div>
                 </div>
                 <div class="col-lg-8 col-sm-12">
                     <div class="card">
-                        <img src="{{ asset('admin/assets/images/signin.svg') }}" alt="Sign In" />
+                        <img src="{{ asset('admin/assets/images/404.svg') }}" alt="404" />
                     </div>
                 </div>
             </div>
@@ -64,5 +56,7 @@
     <script src="assets/bundles/libscripts.bundle.js"></script>
     <script src="assets/bundles/vendorscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js -->
 </body>
+
+</html>
 
 </html>
